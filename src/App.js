40cb393692f4594d7e36f5f80df3a57d3408c8b7
tnals9707 <zzummin9721@gmail.com>
2023-05-16 12:41:02 +0900

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/header';
+import Footer from './components/footer'
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
