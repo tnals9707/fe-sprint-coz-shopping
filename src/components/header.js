d80aa1feb1470b8d5d089f958bff719c2052db7a
tnals9707 <zzummin9721@gmail.com>
2023-05-16 12:19:43 +0900

    create-modal-component

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import Modal from 'react-modal';
 
 
 const Container = styled.div`
@@ -29,30 +30,72 @@ const Logo = styled.div`
     }
 `;
 
-const Wrap = styled.div`
-    position: relative;
-    width: 5rem;
-    height: 1.5rem;
-    > img {
-    position: relative;
+const Hamburger = styled.img`
     cursor: pointer;
-    }
 `;
+
+const modalStyle = {
+    content: {
+        top: '200px',
+        left: 'auto',
+        right: '25px',
+        bottom: 'auto',
+        transform: 'translate(-50%, -50%)',
+        border: 'none',
+        borderRadius: '10px',
+        boxShadow: '0px 8px 8px rgba(0, 0, 0, 0.1)'
+      }
+};
+
+const Nav = styled.ul`
+    display: flex;
+    flex-direction: column;
+    > li {
+        height: 50px;
+        size: 16px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        > img {
+            margin-right: 8px;
+        }
+    }
+`
+
 const Header = () => {
-    const [displayMenu, setDisplayMenu] = useState(false);
-    const dropmenuHandler = () => {
-    setDisplayMenu(!displayMenu);
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const modalHandler = () => {
+    setModalOpen(!modalOpen);
     };
 
     return (
     <Container>
         <Logo>
-            <img src='icon/logo.png' alt='logoImage'></img>
+            <img src='icon/logo.png' alt='logoIcon'></img>
             <h1> COZ Shopping</h1>
         </Logo>
-        <Wrap>
-            <img src='icon/hamburger.png' alt='hambImage'></img>
-        </Wrap>
+        <Hamburger 
+            src='icon/hamburger.png'
+            onClick={modalHandler} 
+        />
+        <Modal
+            isOpen = {modalOpen}
+            onRequestClose={modalHandler}
+            style={modalStyle}>
+                <Nav>
+                    <li>  ⃝ ⃝ ⃝님, 안녕하세요! </li>
+                    <li>
+                        <img src='icon/itemlist.png' alt='itemIcon'></img>
+                        상품리스트 페이지
+                    </li>
+                    <li>
+                        <img src='icon/bookmark.png' alt='bookmarkIcon'></img>
+                        북마크 페이지
+                    </li>
+                </Nav>
+        </Modal>
     </Container>
     );
 };
