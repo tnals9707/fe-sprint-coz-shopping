9b837be9da14cef11f537eb8c22d1f8d20f7b55c
tnals9707 <zzummin9721@gmail.com>
2023-05-17 12:21:49 +0900

    main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header';
 import Footer from './components/footer'
+import Main from './page/main';
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
       <Footer />
     </div>
   );
